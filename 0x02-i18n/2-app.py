fd434a0ba5c22331afb3b727d8e77ef1f28c2d8a
Zeezbaba <Azeez_okeleji>
2024-07-02 15:05:26 +0000

    Flask-Babel: 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Basic Babel Setup"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# flask app configuration
+app.config.from_object(Config)
+# babel = Babel(app)
+
+
+# @babel.localeselector
+def get_locale():
+    """Return the best match language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """return html file"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
